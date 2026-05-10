fbedc478b8be6e57b32c341ab1c0fa71a21b4a95
rcodyp <rahulkr3960@gmail.com>
2026-05-11 00:10:55 +0530

    fixed logo

--- src/components/site/Logo.tsx
+++ src/components/site/Logo.tsx
@@ -3,12 +3,9 @@ import logo from '@/public/logo.png';
 export function Logo({ className = "h-8 w-8" }: { className?: string }) {
   return (
     <img
-      src={logo}
-      alt="Site logo"
+      src="/logo.png"
+      alt="S"
       className={className}
-      onError={(e) => {
-        (e.currentTarget as HTMLImageElement).src = '/logo.svg';
-      }}
     />
   );
 }
